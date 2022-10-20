b0e18baa4de67e327ceac41e8982ebacb5150e67
changshengchong <sen_chong@hotmail.com>
2022-10-20 13:41:22 +0800

    update

--- src/BinarySearch.js
+++ src/BinarySearch.js
@@ -3,23 +3,48 @@ import React, { useRef, useState } from "react";
 import CodeEditor from "@uiw/react-textarea-code-editor";
 
 const BinarySearch = () => {
-  const binarySearch = (arr, l, r, x) => {
-    if (r >= l) {
-      let mid = l + Math.floor((r - l) / 2);
+  const binarySearchRecursive = (arr, start, end, x) => {
+    if (end >= start) {
+      let mid = start + Math.floor((end - start) / 2);
 
-      if (arr[mid] == x) return mid;
+      if (arr[mid] === x) return mid;
 
-      if (arr[mid] > x) return binarySearch(arr, l, mid - 1, x);
+      if (arr[mid] > x) return binarySearchRecursive(arr, start, mid - 1, x);
 
-      return binarySearch(arr, mid + 1, r, x);
+      return binarySearchRecursive(arr, mid + 1, end, x);
     }
 
     return -1;
   };
 
-  const [code, setCode] = useState(
-    "const binarySearch = (arr, l, r, x) => { \n        if (r >= l) { \n            let mid = l + Math.floor((r - l) / 2); \n      \n            if (arr[mid] == x) \n                return mid; \n      \n            if (arr[mid] > x) \n                return binarySearch(arr, l, mid - 1, x); \n      \n            return binarySearch(arr, mid + 1, r, x); \n        } \n      \n        return -1; \n    }"
+  const binarySearchIterative = (arr, x) => {
+    let start = 0;
+    let end = arr.length - 1;
+    while (start <= end) {
+      let mid = Math.floor((end + start) / 2);
+
+      if (arr[mid] === x) return mid;
+      else if (arr[mid] > x) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    }
+
+    return -1;
+  };
+
+  const [recursiveCode, setRecursiveCode] = useState(
+    "const binarySearchRecursive = (arr, start, end, x) => { \n    if (end >= start) { \n      let mid = start + Math.floor((end - start) / 2); \n \n      if (arr[mid] === x) return mid; \n \n      if (arr[mid] > x) return binarySearchRecursive(arr, start, mid - 1, x); \n \n      return binarySearchRecursive(arr, mid + 1, end, x); \n    } \n \n    return -1; \n  };"
   );
+
+  const [iterativeCode, setIterativeCode] = useState(
+    "const binarySearchIterative = (arr, x) => { \n    let start = 0; \n    let end = arr.length - 1; \n    while (start <= end) { \n      let mid = Math.floor((end + start) / 2); \n \n      if (arr[mid] === x) return mid; \n      else if (arr[mid] > x) { \n        end = mid - 1; \n      } else { \n        start = mid + 1; \n      } \n    } \n \n    return -1; \n  };"
+  );
+
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const target = 9;
+  const index = binarySearchIterative(arr, target);
 
   return (
     <VStack
@@ -31,11 +56,27 @@ const BinarySearch = () => {
       borderRadius="md"
     >
       <Text fontWeight="bold">Binary search</Text>
+      <Text>Array: [{arr.toString()}]</Text>
+      <Text>Target: {target}</Text>
+      <Text>Index: {index}</Text>
       <CodeEditor
         language="typescript"
         padding={15}
-        value={code}
-        onChange={(e) => setCode(e.target.value)}
+        value={recursiveCode}
+        onChange={(e) => setRecursiveCode(e.target.value)}
+        style={{
+          width: "100%",
+          fontSize: 15,
+          borderRadius: "1%",
+          fontFamily:
+            "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+        }}
+      />
+      <CodeEditor
+        language="typescript"
+        padding={15}
+        value={iterativeCode}
+        onChange={(e) => setIterativeCode(e.target.value)}
         style={{
           width: "100%",
           fontSize: 15,
